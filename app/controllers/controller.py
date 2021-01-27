7d9919e33a966e4b97f805aff5126adeb707613f
Mark Burns <markburns@gmail.com>
2021-01-27 15:21:12 +0000

    Extension Finished

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -5,3 +5,8 @@ from app.models.shop_app import orders
 @app.route('/orders')
 def index():
     return render_template('index.html', title='Home', orders=orders)
+
+@app.route('/orders/<index>')
+def order(index):
+    customer_order = orders[int(index)]
+    return render_template('order.html', title='Your', order=customer_order)
--- app/models/shop_app.py
+++ app/models/shop_app.py
@@ -1,5 +1,5 @@
 from app.models.order import *
 
-order1 = Order("Jenny", "27th January", 3, "daisy")
-order2 = Order("Mark", "28th January", 4, "pineapple plant")
+order1 = Order("Jenny", "27th January", 3, "daisies")
+order2 = Order("Mark", "28th January", 4, "pineapple plants")
 orders = [order1, order2]
