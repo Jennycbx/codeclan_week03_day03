6c059646f55fc88d11db0b96177c559e9add3ae4
Mark Burns <markburns@gmail.com>
2021-01-27 14:30:57 +0000

    Initial Commit - Webpage Running

---
+++ app/models/order.py
@@ -0,0 +1,7 @@
+class Order():
+    def __init__(self, customer_name, order_date, quantity, plant_name):
+
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.plant_name = plant_name
---
+++ app/models/shop_app.py
@@ -0,0 +1,5 @@
+from app.models.order import *
+
+order1 = Order("Jenny", "27th January", 3, "daisy")
+order2 = Order("Mark", "28th January", 4, "pineapple plant")
+orders = [order1, order2]
